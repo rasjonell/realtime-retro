c6f602dbe86fbd787544b253c6ed1a40c8093f8d
rasjonell <info.gurgen@gmail.com>
2024-12-04 13:21:00 +0400

    Remove Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,21 @@
-import { mdsvex } from 'mdsvex'
-import adapter from '@sveltejs/adapter-vercel';
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [
-    mdsvex({
-      extensions: ['.md']
-    }),
-    vitePreprocess(),
-  ],
+	preprocess: [
+		mdsvex({
+			extensions: ['.md'],
+		}),
+		vitePreprocess(),
+	],
 
-  extensions: ['.svelte', '.md'],
+	extensions: ['.svelte', '.md'],
 
-  kit: {
-    adapter: adapter()
-  }
+	kit: {
+		adapter: adapter(),
+	},
 };
 
 export default config;
